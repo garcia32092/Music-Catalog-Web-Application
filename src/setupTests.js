f33c86e3928d8112d848a2a12d1b03ce3d0ee05c
garcia32092 <pops32092@gmail.com>
2024-06-04 23:33:12 -0700

    configure babel and setupTest.js files

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,11 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+global.scrollTo = jest.fn();
+
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve([]),
+  })
+);
